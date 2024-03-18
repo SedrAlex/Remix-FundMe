ea673f4ed83f3a97ccf62d48a0bf9218f3992c6b
SedrAlex313 <124774403+SedrAlex313@users.noreply.github.com>
2024-03-18 02:49:40 -0400

    native Eth transfer, send, call

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,65 +4,54 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import"./PriceConverter.sol";
 
-interface AggregatorV3Interface {
-
-  function decimals() external view returns (uint8);
-  function description() external view returns (string memory);
-  function version() external view returns (uint256);
-
-  // getRoundData and latestRoundData should both raise "No data present"
-  // if they do not have data to report, instead of returning unset values
-  // which could be misinterpreted as actual reported values.
-  function getRoundData(uint80 _roundId)
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-
-}
 
 contract FundMe {
 
-    uint256 public  minimumUSD = 50;
+  using  PriceConverter for uint256;
 
+    uint256 public  minimumUSD = 50 * 1e18; // 1 * 10 ** 18
+
+    address[] public funders;
+    mapping (address => uint256) public addressToAmountFunded;
+
+      //we want to send an amount fund amount in USD
+      //1. How do we send eth to this contract? 
     function fund() public payable {
-
-           //we want to send an amount fund amount in USD
-           //1. How do we send eth to this contract? 
-
-           require(msg.value > minimumUSD, "Didn't send enough"); //1e18 == 1 * 10 ** 18 =100
-
+           require(msg.value.getConversionRate() >= minimumUSD, "Didn't send enough"); //1e18 == 1 * 10 ** 18 =100 
+           // 18 decimals
+           funders.push(msg.sender);
+           addressToAmountFunded[msg.sender] = msg.value;
+           
            //what is reverting?
            //undo any action before, and send remaining gas back
 }
 
-    function getPrice() public {
-        //ABI
-        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-    }
+function withdraw() public{
 
-    function getVersion() public view returns (uint256){
-          AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-          return  priceFeed.version();
-    }
-    function getConversionRate() public {
+  /*starting index, ending index, step amount*/
+  for( uint256 funderIndex  = 0; funderIndex < funders.length ;funderIndex++){
 
-    }
+    address funder = funders[funderIndex];
+    addressToAmountFunded[funder] = 0;
+  }
+  //reset the array
+    funders = new address[](0);
+  //actually withdraw the funds
+
+  //transfer
+  payable(msg.sender).transfer(address(this).balance);
+  //send
+  bool sendSuccess = payable(msg.sender).send(address(this).balance);
+  require(sendSuccess,"send failed");
+  // this is the most favourabile approach that is used to send native Etherium
+  //call
+    (bool callSuccess,)= payable(msg.sender).call{value: address(this).balance}("");
+    require(callSuccess, "call failed");
+
+}
+    
     }
  
